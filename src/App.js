f8657a4284d7aa25d1dc2bed3d9b4a1de72f2dfd
Soumitra Mandal <soumitramandal1999@gmail.com>
2020-07-12 21:34:18 +0530

    some practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function Welcome(props){
+  return <h1>Welcome {props.name}</h1>
+}
+
+
+function Goodbye(p){
+return <h1>Goodbye {p.name}</h1>
+}
+
+const me = <Goodbye name = 'Mandal' />
+const el  = <Welcome name = 'Soumitra' />
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {el}
+    {me}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
