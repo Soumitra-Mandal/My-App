75ed3c96c22da6616a22e456ecc445e35f21cc7c
Soumitra Mandal <soumitramandal1999@gmail.com>
2020-07-22 19:39:31 +0530

    haha

--- src/App.js
+++ src/App.js
@@ -4,61 +4,19 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { connect } from 'react-redux';
 
-
-// Action creator
-function incrementCounter(num) {
-  return { type: 'INCREMENT', num: num }
-}
-function decrementCounter(num){
-  return {type:'DECREMENT' , num:num}
-}
-const initialState = {
-  count: 0
-};
-// Reducer function
-function reducer(state = initialState, action) {
-  switch(action.type) {
-    case 'INCREMENT':
-      return { count: state.count + action.num };
-    case 'DECREMENT':
-      return {count: state.count - action.num};
-    default:
-      return state;
-  }
-}
-function Counter(props) {
-  function handleClick() {
-    props.incrementCounter(1);
-  }
-  function handleClick2(){
-    props.decrementCounter(1);
-  }
-
-    return <div>
-    <p>{props.count}</p>
-    <button onClick={handleClick}>Increment</button>
-    <button onClick={handleClick2}>Decrement</button>
-    </div>;
-}
-
-function mapStateToProps(state) {
-  return {
-    count: state.count
-  };
-}
-const mapDispatchToProps = {
-  incrementCounter,
-  decrementCounter
-}
-
-const store = createStore(reducer);
-
-const Counters = connect(mapStateToProps,mapDispatchToProps)(Counter);
-
-const el = <Provider store={store}>
-          <Counters/>
-        </Provider>;
-
+const el = 
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+;
 function App() {
   return (
     <div className="App">
