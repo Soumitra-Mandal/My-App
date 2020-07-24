4361745ae6dd843e30149be3a73f9241a3709222
Soumitra Mandal <soumitramandal1999@gmail.com>
2020-07-24 11:12:45 +0530

    whatever

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import './App.css';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { connect } from 'react-redux';
+import {useState} from 'react';
+//import { Provider } from 'react-redux';
+//import { createStore } from 'redux';
+//import { connect } from 'react-redux';
 
-const el = 
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
-;
+function Counter(){
+    const [count,setCounter] = useState(Math.floor(Math.random()*1000));
+  
+  function increment(){
+    setCounter(count+1);
+  }
+  function decrement(){
+    setCounter(count-1);
+  }
+  function multiply() {
+    if(Math.abs(count*2) >1000){
+      setCounter(1000);
+    }
+    else setCounter(count*2);
+    
+  }
+  function reset() {
+    setCounter(0);
+  }
+
+    return <div><h1>{count}</h1>
+    <button onClick = {increment}>Increment</button>
+    <button onClick={decrement}>Decrement</button>
+    <button onClick={multiply}>Double</button>
+    <button onClick={reset}>reset</button>
+    </div>;
+  }
+
+
 function App() {
   return (
     <div className="App">
-      {el}
+      <Counter/>
     </div>
   );
 }
