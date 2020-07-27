9ade79e3165383decc6e1c6838c46a0a8f8390ae
Soumitra Mandal <soumitramandal1999@gmail.com>
2020-07-27 10:39:28 +0530

    animation added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,39 @@
 import React from 'react';
 import './App.css';
-import {useState} from 'react';
+//import {useState} from 'react';
 //import { Provider } from 'react-redux';
 //import { createStore } from 'redux';
 //import { connect } from 'react-redux';
 
-function Counter(){
-    const [count,setCounter] = useState(Math.floor(Math.random()*1000));
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
   
-  function increment(){
-    setCounter(count+1);
-  }
-  function decrement(){
-    setCounter(count-1);
-  }
-  function multiply() {
-    if((count*2)>=1000){
-      setCounter(1000);
-    }
-    else if(count*2<=-1000){
-      setCounter(-1000);
-    }
-    else setCounter(count*2);
-    
-  }
-  function reset() {
-    setCounter(0);
-  }
+  changeColor = () => {
+    if(this.state.favoritecolor==='red')
+    this.setState({favoritecolor: "blue"});
+    else
+    this.setState({favoritecolor: "red"});
 
-    return <div><h1>{count}</h1>
-    <button onClick = {increment}>Increment</button>
-    <button onClick={decrement}>Decrement</button>
-    <button onClick={multiply}>Double</button>
-    <button onClick={reset}>reset</button>
-    </div>;
+
   }
+  render() {
+    return (
+      <div>
+      <h1><span>My Favorite Color is {this.state.favoritecolor}</span></h1>
+      <button type="button" onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }
+}
 
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <Header/>
     </div>
   );
 }
